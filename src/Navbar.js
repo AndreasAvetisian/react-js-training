4f7aed4a31ddb9189c0e2ed2d12de9ced100bb05
Andreas Avetisian <t0avan00@students.oamk.fi>
2023-01-23 00:36:35 +0100

    Added styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
             <h1>My blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New blog</a>
+                <a href="/create" className="newBlogButton">New blog</a>
             </div>
         </nav>
      );
