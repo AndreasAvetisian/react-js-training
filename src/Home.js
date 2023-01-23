b2cb5477ff961c27709a0d0b791c365d72324ddf
Andreas Avetisian <t0avan00@students.oamk.fi>
2023-01-23 10:22:16 +0100

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const  handleClick = () => {
+        console.log('hello');
+    }
+
+    const  handleClickAgain = (name) => {
+        console.log('Hello ' + name);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('Andreas')}>Click me again</button>
         </div>
     );
 }
